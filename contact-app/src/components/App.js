9ea0a521e0f8a392158e551ace2dbf169df64b7e
Akash <akashkumarsingh11032001@gmail.com>
2023-02-26 18:13:48 +0530

    base update

---
+++ contact-app/src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import ContactList from './ContactList';
+import AddContact from './AddContact';
+
+function App() {
+  return (
+    <div>
+      {/* <h1>Hello World!</h1> */}
+      <Header />
+      {/* <AddContact />
+      <ContactList /> */}
+    </div>
+  );
+}
+
+export default App;
